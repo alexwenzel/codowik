ea371e05f710f58264003d62ae004e1951cea6fe
alexwenzel <alexander.wenzel.berlin@gmail.com>
2014-01-15 16:05:27 +0100

    bugfix: mediamamager

--- mediamanager.php
+++ mediamanager.php
@@ -5,6 +5,10 @@
  * @author   Andreas Gohr <andi@splitbrain.org>
  * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
+
+// require functions
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR .'bootstrap.php');
+
 // must be run from within DokuWiki
 if (!defined('DOKU_INC')) die();
 header('X-UA-Compatible: IE=edge,chrome=1');
